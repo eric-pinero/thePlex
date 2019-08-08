99f92a4ec5453d6b70655acc93ba884d45f88071
Eric Pinero <ericpinero@gmail.com>
2019-08-08 12:00:09 -0400

    create database and users table

--- frontend/thePlex.jsx
+++ frontend/thePlex.jsx
@@ -1,11 +1,7 @@
-npm install --save webpack
-webpack-cli
-react
-react-dom
-react-router-dom
-redux
-react-redux
-@babel/core
-@babel/preset-react
-@babel/preset-env
-babel-loader
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+});
